cd5800ee4b188d12993a6c0dc4d0d3f59a987552
gryng <vaiomx56@gmail.com>
2020-07-21 22:35:21 +0900

    分岐

---
+++ main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+  console.log("start");
+
+  $('input.js-choice-btn').change(function(){
+
+    // クリックしたinputのdataを確認する
+    //console.log($(this).data().scene);
+
+    var scene_num = $(this).data().scene;
+    var route_type = $(this).data().route;
+
+    // シーンを表示
+    $("div[data-scene=" + scene_num + "]").removeClass("m-scene--hidden");
+
+    // シーン内のルートを全部非表示
+    $("div[data-scene=" + scene_num + "]").find("div[data-route]").addClass("m-scene__route--hidden");
+
+    // 該当ルートを表示
+    $("div[data-scene=" + scene_num + "]").find("div[data-route=" + route_type + "]").removeClass("m-scene__route--hidden");
+
+
+  });
+});
